d9878daa78378f79a01d3fb23f83372520537578
João Camillo de Moura <joaocamo@gmail.com>
2024-08-27 18:33:46 -0300

    0.9.2-u96

--- Assets/Game/Scenes/Menus/Main_Menu/Scripts/MainMenuController.cs
+++ Assets/Game/Scenes/Menus/Main_Menu/Scripts/MainMenuController.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 using Game.Navigation;
 
 namespace Game.Menus
@@ -6,10 +8,21 @@ namespace Game.Menus
     public class MainMenuController : MonoBehaviour
     {
         [SerializeField] private SceneNavigationInfo[] sceneNavigationInfos;
+        [SerializeField] private RectTransform backgroundTransform;
+
+        private Coroutine _animationCoroutine = null;
+        private bool _canAnimate = false;
+
+        private const int ANIMATION_DELAY = 60;
+        private readonly WaitForSeconds _animationDelay = new WaitForSeconds(60);
+
+        private readonly Vector3 INITIAL_POSITION = Vector3.zero;
+        private readonly Vector3 FINAL_POSITION = new Vector3(0,-960);
 
         private void Awake()
         {
             LoadNavigation();
+            StartAnimation();
         }
 
         private void LoadNavigation()
@@ -17,6 +30,33 @@ namespace Game.Menus
             foreach (SceneNavigationInfo sceneNavigation in sceneNavigationInfos)
             {
                 sceneNavigation.button.onClick.AddListener(() => NavigationController.RequestSceneLoad(sceneNavigation.scene, sceneNavigation.loadSceneMode, sceneNavigation.hasLoading));
+            }
+        }
+
+        private void StartAnimation()
+        {
+            _canAnimate = false;
+
+            if (_animationCoroutine != null)
+            {
+                StopCoroutine(_animationCoroutine);
+                _animationCoroutine = null;
+            }
+
+            _canAnimate = true;
+            _animationCoroutine = StartCoroutine(BackgroundAnimation());
+        }
+
+        private IEnumerator BackgroundAnimation()
+        {
+            while(_canAnimate)
+            {
+                backgroundTransform.DOLocalMove(FINAL_POSITION, ANIMATION_DELAY).SetEase(Ease.Linear);
+
+                yield return _animationDelay;
+
+                backgroundTransform.DOKill();
+                backgroundTransform.localPosition = INITIAL_POSITION;
             }
         }
     }
